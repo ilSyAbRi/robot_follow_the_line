6796ca937df5c3bca2f2b9d70203bdce019b1f65
ilSyAbRi <iliasssyabri1@gmail.com>
2026-06-30 19:55:27 +0100

    last push

--- robot.cpp
+++ robot.cpp
@@ -1,177 +1,113 @@
-// // ===============================
-// // L298N Motor Driver - 2WD Robot
-// // ===============================
+// ======================================
+// SIMPLE LINE FOLLOWER
+// ======================================
 
-// Motor A
-const int ENA = 5;   // PWM
-const int IN1 = 8;
-const int IN2 = 9;
+// -------- MOTOR PINS --------
+const int ENA = 5;
+const int IN1 = 2;
+const int IN2 = 3;
 
-// Motor B
-const int ENB = 6;   // PWM
-const int IN3 = 10;
-const int IN4 = 11;
+const int ENB = 6;
+const int IN3 = 4;
+const int IN4 = 7;
 
-// // ===============================
+// -------- SENSOR PINS --------
+const int S2 = A0;  
+const int S3 = A1;
 
-// void setup() {
-//    pinMode(13, OUTPUT);
-//   pinMode(IN1, OUTPUT);
-//   pinMode(IN2, OUTPUT);
+// -------- SETTINGS --------
+const int THRESHOLD = 500;
 
-//   pinMode(ENB, OUTPUT);
-//   pinMode(IN3, OUTPUT);
-//   pinMode(IN4, OUTPUT);
+const int BASE_SPEED = 150;
+const int CORRECTION = 40;
 
-//   stopMotors();
-//  }
+// ======================================
 
-// // ===============================
-
-// void loop() {
-
-//   // Move forward
-//   forward(200);
-//   delay(2000);
-
-//   // Stop
-//   stopMotors();
-//   delay(1000);
-
-//   // Move backward
-//   backward(200);
-//   delay(2000);
-
-//   // Stop
-//   stopMotors();
-//   delay(1000);
-
-//   // Turn left
-//   left(200);
-//   delay(1000);
-
-//   // Stop
-//   stopMotors();
-//   delay(1000);
-
-//   // Turn right
-//   right(200);
-//   delay(1000);
-
-//   // Stop
-//   stopMotors();
-//   delay(3000);
-// }
-
-// // ===============================
-// // Movement Functions
-// // ===============================
-
-void forward(int speed) {
-  analogWrite(ENA, speed);
-  analogWrite(ENB, speed);
-
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-}
-
-// void backward(int speed) {
-//   analogWrite(ENA, speed);
-//   analogWrite(ENB, speed);
-
-//   digitalWrite(IN1, LOW);
-//   digitalWrite(IN2, HIGH);
-
-//   digitalWrite(IN3, LOW);
-//   digitalWrite(IN4, HIGH);
-// }
-
-// void left(int speed) {
-//   analogWrite(ENA, speed);
-//   analogWrite(ENB, speed);
-
-//   // Left motor backward
-//   digitalWrite(IN1, LOW);
-//   digitalWrite(IN2, HIGH);
-
-//   // Right motor forward
-//   digitalWrite(IN3, HIGH);
-//   digitalWrite(IN4, LOW);
-// }
-
-// void right(int speed) {
-//   analogWrite(ENA, speed);
-//   analogWrite(ENB, speed);
-
-//   // Left motor forward
-//   digitalWrite(IN1, HIGH);
-//   digitalWrite(IN2, LOW);
-
-//   // Right motor backward
-//   digitalWrite(IN3, LOW);
-//   digitalWrite(IN4, HIGH);
-// }
-
-// void stopMotors() {
-//   analogWrite(ENA, 0);
-//   analogWrite(ENB, 0);
-
-//   digitalWrite(IN1, LOW);
-//   digitalWrite(IN2, LOW);
-
-//   digitalWrite(IN3, LOW);
-//   digitalWrite(IN4, LOW);
-// }
-
-const int LDR = A0;
-int flag_light = 0;
-int verification_light_start(int LDR)
+void setup()
 {
-  if (LDR > 100)
-    return 0;
-  else
-    return 1;
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
 
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+
+    pinMode(ENA, OUTPUT);
+    pinMode(ENB, OUTPUT);
+
+    Serial.begin(9600);
+
+    // Motors always move forward
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
 }
-void setup() {
-  Serial.begin(9600);
+
+// ======================================
+
+void loop()
+{
+    int leftValue  = analogRead(S2);
+    int rightValue = analogRead(S3);
+
+    bool leftBlack  = leftValue < THRESHOLD;
+    bool rightBlack = rightValue < THRESHOLD;
+
+    // Default speed
+    int leftMotor  = BASE_SPEED;
+    int rightMotor = BASE_SPEED;
+
+    // -----------------------------
+    // Normal state
+    // Both sensors see white
+    // -----------------------------
+    if (!leftBlack && !rightBlack)
+    {
+        // Keep going
+    }
+
+    // -----------------------------
+    // Robot moved left
+    // S2 sees black
+    // Turn right a little
+    // -----------------------------
+    else if (leftBlack && !rightBlack)
+    {
+        leftMotor  += CORRECTION;
+        rightMotor -= CORRECTION;
+    }
+
+    // -----------------------------
+    // Robot moved right
+    // S3 sees black
+    // Turn left a little
+    // -----------------------------
+    else if (!leftBlack && rightBlack)
+    {
+        leftMotor  -= CORRECTION;
+        rightMotor += CORRECTION;
+    }
+
+    // -----------------------------
+    // Both black
+    // Ignore for now
+    // -----------------------------
+    else
+    {
+        // We'll use this later for intersections.
+    }
+
+    // Send speeds to motors
+    analogWrite(ENA, leftMotor);
+    analogWrite(ENB, rightMotor);
+
+    // Debug
+    Serial.print("S2: ");
+    Serial.print(leftValue);
+
+    Serial.print("   S3: ");
+    Serial.println(rightValue);
+
+    delay(5);
 }
-
-void loop() {
-// int value = analogRead(LDR);
-
-//     Serial.print("Value: ");
-//     Serial.print(value);
-
-//     Serial.print(" | Light: ");
-
-//     if (value > 100) {
-//     Serial.println("YES");
-//   } else {
-//     Serial.println("NO");
- // }
-
-  if (flag_light == 0)
-  {
-    flag_light = verification_light_start(analogRead(LDR));
-  }
-  else
-  {
-  //   int value = analogRead(LDR);
-
-  //   Serial.print("Value: ");
-  //   Serial.print(value);
-
-  //   Serial.print(" | Light: ");
-
-  //   if (value > 1000) {
-  //   Serial.println("YES");
-  // } else {
-  //   Serial.println("NO");
-  // }
-forward(255);
-
-  }}
